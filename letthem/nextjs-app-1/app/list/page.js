dd149a321048f30add02e2206c0e277f2e005bae
letthem <suyeon3662@naver.com>
2024-04-02 15:32:57 +0900

    feat: 10_Array & Object state

--- letthem/nextjs-app-1/app/list/page.js
+++ letthem/nextjs-app-1/app/list/page.js
@@ -2,9 +2,9 @@
 
 import { useState } from "react";
 
-export default function Home() {
+export default function List() {
   let 상품 = ["Tomatoes", "Pasta", "Coconut"];
-  let [수량, 수량변경] = useState(0);
+  let [수량, 수량변경] = useState([0, 0, 0]);
   return (
     <div>
       <h4 className="title">상품목록</h4>
@@ -13,10 +13,21 @@ export default function Home() {
           <div className="food" key={i}>
             <img src={`/food${i}.png`} className="food-img" />
             <h4>{a} $40</h4>
-            <span> {수량} </span>
             <button
               onClick={() => {
-                수량변경(수량 + 1);
+                let copy = [...수량];
+                copy[i]--;
+                수량변경(copy);
+              }}
+            >
+              -
+            </button>
+            <span> {수량[i]} </span>
+            <button
+              onClick={() => {
+                let copy = [...수량];
+                copy[i]++;
+                수량변경(copy);
               }}
             >
               +
