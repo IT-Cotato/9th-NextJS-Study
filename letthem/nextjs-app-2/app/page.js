eb408dd42d6f54d75e208fc9b85b76558647a982
letthem <suyeon3662@naver.com>
2024-04-02 17:03:27 +0900

    feat: 13_Retrieve Data

--- letthem/nextjs-app-2/app/page.js
+++ letthem/nextjs-app-2/app/page.js
@@ -2,8 +2,7 @@ import { connectDB } from "@/util/database";
 import { MongoClient } from "mongodb";
 
 export default async function Home() {
-  const client = await connectDB;
-  const db = client.db("forum");
+  const db = (await connectDB).db("forum");
   let result = await db.collection("post").find().toArray();
   console.log(result);
 
