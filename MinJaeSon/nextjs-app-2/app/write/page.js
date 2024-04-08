f29472481a58e35e6583fa5f0321f0115b04d22c
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-04-09 03:52:07 +0900

    feat: 글 작성 기능 완성

--- MinJaeSon/nextjs-app-2/app/write/page.js
+++ MinJaeSon/nextjs-app-2/app/write/page.js
@@ -2,7 +2,7 @@ export default function Write() {
   return (
     <div>
       <h4>글 작성</h4>
-      <form action="/api/post" method="post">
+      <form action="/api/post/new" method="post">
         <input type="text" name="title" placeholder="제목 입력" />
         <input type="text" name="content" placeholder="내용 입력" />
         <button type="submit">버튼</button>
