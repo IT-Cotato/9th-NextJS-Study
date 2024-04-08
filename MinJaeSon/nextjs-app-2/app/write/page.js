48bec1902d257290e86d2421b5d86bb414306874
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-04-09 03:21:31 +0900

    feat: 서버 기능 개발 숙제1

--- MinJaeSon/nextjs-app-2/app/write/page.js
+++ MinJaeSon/nextjs-app-2/app/write/page.js
@@ -2,7 +2,7 @@ export default function Write() {
   return (
     <div>
       <h4>글 작성</h4>
-      <form action="/api/test" method="post">
+      <form action="/api/list" method="get">
         <button type="submit">버튼</button>
       </form>
     </div>
