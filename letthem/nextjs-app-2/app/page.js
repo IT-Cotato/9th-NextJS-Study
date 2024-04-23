21e297b16b613aae8c8b34dc831df16f9a23e216
letthem <suyeon3662@naver.com>
2024-04-23 16:51:27 +0900

    feat: 23_Static, dynamic rendering, cache

--- letthem/nextjs-app-2/app/page.js
+++ letthem/nextjs-app-2/app/page.js
@@ -1,6 +1,8 @@
 import { connectDB } from "@/util/database";
 import { MongoClient } from "mongodb";
 
+export const revalidate = 60; // 해당 페이지 방문시 60초동안 캐싱됨
+
 export default async function Home() {
   const db = (await connectDB).db("forum");
   let result = await db.collection("post").find().toArray();
