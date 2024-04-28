aae5902f734a1df23b547e6c790413f00929e008
WONYOUNG-HC <whdnjsdud551@gmail.com>
2024-04-28 23:19:25 +0900

    [조원영] 2주차 과제

---
+++ WONYOUNG-HC/nextjs-app-1/pages/list/index.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import 작명 from "/public/food0.png";
+
+("use client");
+export default function Home() {
+  let 상품 = ["토마토", "파스타", "코코넛"];
+  let [수량, 수량변경] = useState([0, 0, 0]);
+
+  return (
+    <div>
+      <h4 className="title">상품목록</h4>
+      {상품.map((a, i) => {
+        return (
+          <div className="food" key={i}>
+            <img src={`/food${i}.png`} className="food-img" />
+            <h4>{a} $40</h4>
+            <span>{수량[i]}</span>
+            <button
+              onClick={() => {
+                let copy = [...수량];
+                copy[i]++;
+                수량변경(copy);
+              }}
+            >
+              +
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
