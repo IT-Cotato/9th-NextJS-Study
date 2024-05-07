a101cfc45e7afa0dee2113de89a5b026b1b9d293
WONYOUNG-HC <whdnjsdud551@gmail.com>
2024-05-07 21:47:23 +0900

    [조원영] 5주차 과제

--- WONYOUNG-HC/forum/src/pages/api/post/new.js
+++ WONYOUNG-HC/forum/src/pages/api/post/new.js
@@ -1,6 +1,14 @@
 import { connectDB } from "@/util/database";
+import { getSession } from "next-auth/react";
+import { authOptions } from "../auth/[...nextauth]";
 
 export default async function handler(요청, 응답) {
+  let session = await getSession(요청, 등답, authOptions);
+
+  if (session) {
+    요청.body.author = session.user.name;
+  }
+
   if (요청.method === "POST") {
     if (요청.body.title === "") {
       return 응답.status(500).json("제목ㄱ");
