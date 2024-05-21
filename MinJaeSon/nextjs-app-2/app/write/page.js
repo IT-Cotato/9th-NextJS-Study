dfc1c5139a85ec70c22e27a8325a72041bde932f
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-05-21 13:25:27 +0900

    [손민재] 7주차 과제 (#32)
    * feat: 7주차 실습과제
    
    * docs: 7주차 내용정리

--- MinJaeSon/nextjs-app-2/app/write/page.js
+++ MinJaeSon/nextjs-app-2/app/write/page.js
@@ -1,12 +1,47 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Write() {
+  let [src, setSrc] = useState("");
+
   return (
     <div>
       <h4>글 작성</h4>
       <form action="/api/post/new" method="post">
         <input type="text" name="title" placeholder="제목 입력" />
         <input type="text" name="content" placeholder="내용 입력" />
+        <input
+          type="file"
+          accept="image/*"
+          onChange={async (e) => {
+            let file = e.target.files[0];
+            let filename = encodeURIComponent(file.name);
+            let res = await fetch(`/api/post/image?file=${filename}`);
+            res = await res.json();
+            console.log(res);
+
+            //S3 업로드
+            const formData = new FormData();
+            Object.entries({ ...res.fields, file }).forEach(([key, value]) => {
+              formData.append(key, value);
+            });
+            let result = await fetch(res.url, {
+              method: "POST",
+              body: formData,
+            });
+            console.log(result);
+
+            if (result.ok) {
+              setSrc(result.url + "/" + filename);
+            } else {
+              console.log("업로드 실패");
+            }
+          }}
+        />
+        <img src={src} />
         <button type="submit">발행</button>
       </form>
     </div>
-  )
+  );
 }
